61ffaf12ede77fb0f7453020a24f176ba26e66ef
drei <andredbenedetti@gmail.com>
2023-04-20 15:07:48 -0300

    Update print-npm-scripts.sh

--- print-npm-scripts.sh
+++ print-npm-scripts.sh
@@ -1,2 +1,3 @@
 # if package.json exists, print available scripts
+# TODO: use this in a way that it runs after doing a "cd"
 [ -f ./package.json ] && echo "package.json scripts:" && cat package.json | jq '.scripts'
